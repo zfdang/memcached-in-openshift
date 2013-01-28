8ff98ae2aa47ef5febdf1439eac924cb7aca9578
zhengfa dang <dantifer@gmail.com>
2013-01-28 15:53:46 +0800

    change default digits and timeout

--- wsgi/pi.py
+++ wsgi/pi.py
@@ -7,9 +7,9 @@ from decorators import cached
 bp = Blueprint('pi', __name__)
 
 
-@bp.route("/", defaults={'digits': 50000})
+@bp.route("/", defaults={'digits': 20000})
 @bp.route("/<int:digits>/")
-@cached(30)
+@cached(180)
 def index(digits):
     pi = pi_chudnovsky_bs(digits)
     return render_template("pi.html", pi=pi, digits=digits)
